f34182032d166440198567f0b34373ba98907c24
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-02 13:05:35 +0200

    Add serializer class

---
+++ app/serializers/doctorSerializer.rb
@@ -0,0 +1,28 @@
+class doctorSerializer
+
+  def initialize(doctor: nil, user:)
+    @doctor = doctor
+    @user = user
+  end
+
+  def serialize_new_doctor()
+    serialized_new_doctor = serialize_doctor(@doctor).merge(user_details)
+    serialized_new_doctor.to_json()
+  end
+  
+  private def serialize_doctor(doctor)
+    {
+      doctor: {
+        id: doctor.id,
+        image_url: doctor.get_image_url(),
+        name: doctor.name,
+        created_at: doctor.created_at,
+        client: {
+          id: doctor.user.id,
+          name: doctor.user.name,
+        }
+      }
+    }
+  end
+  
+end
