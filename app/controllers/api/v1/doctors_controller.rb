4fc111328649e406ff7d45e6921395bc98345816
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-03 10:56:56 +0200

    Test authentication

---
+++ app/controllers/api/v1/doctors_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class Api::V1::DoctorsController < ApplicationController
+      def index
+        @doctors = Doctor.all
+        render json: @doctors
+      end
+    
+      def create
+        @user = get_current_user()
+        params[:user_id] = @user.id
+    
+        @doctor = Doctor.create(post_params())
+        respond_to_post()
+      end
+    
+      private def doctor_params
+        params.permit(:client_id, :name, :email, :image)
+      end
+    
+      private def respond_to_doctor()
+        if @doctor.valid?()
+          doctor_serializer = PostSerializer.new(doctor: @doctor, user: @user)
+          render json: doctor_serializer.serialize_new_doctor()
+        else
+          render json: { errors: doctor.errors }, status: 400
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
   post 'authenticate', to: 'authentication#authenticate'
-  resources :doctor, only: %i[index create]
+  namespace :api do
+    namespace :v1 do
+      resources :doctors, only: %i[index create] do
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
