aee07213e7734d44619b300fe305ee20182915bb
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-03 10:13:30 +0200

    Add home routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#index'
   post 'authenticate', to: 'authentication#authenticate'
   resources :doctor, only: %i[index create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
