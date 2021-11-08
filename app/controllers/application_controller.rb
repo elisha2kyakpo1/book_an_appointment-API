c2d0cd26bf872706b2829125d3c58af4270ad113
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-08 09:57:33 +0200

    Remove application controller before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_request, except: %i[create]
+  # before_action :authenticate_request, except: %i[create]
    attr_reader :current_client
  
    private
